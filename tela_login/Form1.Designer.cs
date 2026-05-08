69d6d43ad97b7411450cff1d8874df1e2d09c656
Mysdeiza <mysdeiza22@gmail.com>
2026-05-07 22:36:33 -0300

    Initial commit

---
+++ tela_login/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace tela_login
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.lbEmail = new System.Windows.Forms.Label();
+            this.lbSenha = new System.Windows.Forms.Label();
+            this.btnLogin = new System.Windows.Forms.Button();
+            this.btnCadLogin = new System.Windows.Forms.Button();
+            this.lbLogin = new System.Windows.Forms.Label();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.SuspendLayout();
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtEmail.Location = new System.Drawing.Point(83, 99);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(245, 31);
+            this.txtEmail.TabIndex = 0;
+            // 
+            // txtSenha
+            // 
+            this.txtSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSenha.Location = new System.Drawing.Point(83, 151);
+            this.txtSenha.Name = "txtSenha";
+            this.txtSenha.Size = new System.Drawing.Size(245, 31);
+            this.txtSenha.TabIndex = 1;
+            // 
+            // lbEmail
+            // 
+            this.lbEmail.AutoSize = true;
+            this.lbEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbEmail.Location = new System.Drawing.Point(13, 106);
+            this.lbEmail.Name = "lbEmail";
+            this.lbEmail.Size = new System.Drawing.Size(64, 20);
+            this.lbEmail.TabIndex = 2;
+            this.lbEmail.Text = "E-mail:";
+            // 
+            // lbSenha
+            // 
+            this.lbSenha.AutoSize = true;
+            this.lbSenha.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbSenha.Location = new System.Drawing.Point(11, 158);
+            this.lbSenha.Name = "lbSenha";
+            this.lbSenha.Size = new System.Drawing.Size(66, 20);
+            this.lbSenha.TabIndex = 3;
+            this.lbSenha.Text = "Senha:";
+            // 
+            // btnLogin
+            // 
+            this.btnLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLogin.Location = new System.Drawing.Point(83, 198);
+            this.btnLogin.Name = "btnLogin";
+            this.btnLogin.Size = new System.Drawing.Size(84, 34);
+            this.btnLogin.TabIndex = 4;
+            this.btnLogin.Text = "Login";
+            this.btnLogin.UseVisualStyleBackColor = true;
+            // 
+            // btnCadLogin
+            // 
+            this.btnCadLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCadLogin.Location = new System.Drawing.Point(225, 198);
+            this.btnCadLogin.Name = "btnCadLogin";
+            this.btnCadLogin.Size = new System.Drawing.Size(103, 34);
+            this.btnCadLogin.TabIndex = 5;
+            this.btnCadLogin.Text = "Cadastre-se";
+            this.btnCadLogin.UseVisualStyleBackColor = true;
+            this.btnCadLogin.Click += new System.EventHandler(this.btnCadLogin_Click);
+            // 
+            // lbLogin
+            // 
+            this.lbLogin.AutoSize = true;
+            this.lbLogin.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbLogin.Location = new System.Drawing.Point(167, 32);
+            this.lbLogin.Name = "lbLogin";
+            this.lbLogin.Size = new System.Drawing.Size(52, 16);
+            this.lbLogin.TabIndex = 7;
+            this.lbLogin.Text = "LOGIN";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(387, 450);
+            this.Controls.Add(this.lbLogin);
+            this.Controls.Add(this.btnCadLogin);
+            this.Controls.Add(this.btnLogin);
+            this.Controls.Add(this.lbSenha);
+            this.Controls.Add(this.lbEmail);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.txtEmail);
+            this.ForeColor = System.Drawing.Color.DarkOliveGreen;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.Label lbEmail;
+        private System.Windows.Forms.Label lbSenha;
+        private System.Windows.Forms.Button btnLogin;
+        private System.Windows.Forms.Button btnCadLogin;
+        private System.Windows.Forms.Label lbLogin;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+    }
+}
+
---
+++ tela_login/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+
+namespace tela_login
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadLogin_Click(object sender, EventArgs e)
+        {
+            string campoLogin = txtEmail.Text;
+            int controleLinhasAfetadas = 0;
+
+            string dadosConexao = "server=localhost;user=root;password=;database=ecoeficiencia";
+            using (MySqlConnection conn = new MySqlConnection(dadosConexao))
+            { //utilizo das informações
+                conn.Open();
+                string scriptInsert = "INSERT INTO tb_acesso (Login) VALUE (@Login)";
+
+                using (MySqlCommand comando = new MySqlCommand(scriptInsert, conn))
+                {
+                    comando.Parameters.AddWithValue("@Login", campoLogin);
+
+                    controleLinhasAfetadas = comando.ExecuteNonQuery();
+                }
+                conn.Close();
+            }//MySqlConnection
+
+            if (controleLinhasAfetadas > 0)
+                {
+                    MessageBox.Show("Dados salvo com sucesso!");
+
+                }
+            else
+                {
+                    MessageBox.Show("Ooops algo deu errado");
+                }
+           
+        }
+    }
+}
